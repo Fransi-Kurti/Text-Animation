19b9d03f4ef00699bb9519f6789d1dac1b65cfd9
fransi-kurti <fransi.kurti@gmail.com>
2023-02-18 22:22:09 +0100

    text animation update javascript and css

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const containerEl = document.getElementById("container");
 
-containerEl.innerHTML = `
-<h1>Cats Are Cute</h1>
-`
+const catsAreAmaizing = ["Cute", "Beautiful", "Nice", "Lovely", "Squishy", "Better than Dogs"];
+
+let catsAreAmaizingIndex = 0;
+
+let characterIndex = 0;
+
+updateNewWord();
+
+function updateNewWord(){
+    characterIndex++;
+    containerEl.innerHTML = `
+    <h1>Cats Are ${catsAreAmaizing[catsAreAmaizingIndex].slice(0,characterIndex)}</h1>
+    `;
+    if(characterIndex === catsAreAmaizing[catsAreAmaizingIndex].length){
+        catsAreAmaizingIndex++
+        characterIndex = 0;
+    }
+    setTimeout(updateNewWord, 400);
+}
+
